3066f87ed52efd308e4ae9414fd4bc8cdd124fe1
killerstoner002 <111683601+killerstoner002@users.noreply.github.com>
2022-09-09 03:59:32 +0530

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,54 @@
+import random
+
+while True:
+    choices = ["rock","paper","scissors"]
+
+    computer = random.choice(choices)
+    player= None
+    
+    while player not in choices:
+        player= input("rock, paper or scissors?: ").lower()
+
+    if player == computer: 
+        print("computer: ",computer)
+        print("player: ",player)
+        print("tie!!")
+        
+    elif player == "rock":
+        if computer == "paper":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("U Lose :(")
+        if computer == "scissors":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("U Win :)")
+            
+    elif player == "scissors":
+        if computer == "rock":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("U Lose :(")
+        if computer == "paper":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("U Win :)")
+            
+    elif player == "paper":
+        if computer == "scissors":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("U Lose :(")
+        if computer == "rock":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("U Win :)")
+
+    play_again = input("Play again? (yes/no): ").lower()
+
+    if play_again != "yes":      
+        break
+print("Bye!")
+
+
+        
